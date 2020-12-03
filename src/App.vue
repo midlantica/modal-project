830993d21b81240b625ab168e011435de22a7354
Drew Harper <druharper@gmail.com>
2020-12-02 18:54:16 -0600

    modals

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click.prevent="handleClick">Click me!</button>
+
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal" />
+  </div>
+
+  <br><br>
+  <div>
+    <button @click="toggleModal">Open Modal</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal },
+  data() {
+    return {
+      title: "Drew is a fun guy :)",
+      header:"Sign up for the Giveaway",
+      text: "Grab the giveaway for half price!",
+      showModal: false
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('reddy')
+      this.$refs.name.focus()
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
   }
 }
 </script>
@@ -22,5 +48,9 @@ export default {
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+  }
+
+  .reddy {
+    background: rgb(251, 255, 197);
   }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="backdrop" @click="closeModal">
+    <div class="modal" :class="{sale:theme === 'sale'}">
+      <h1>{{header}}</h1>
+      <p>{{text}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Modal',
+    props: [
+      'header',
+      'text',
+      'theme'
+    ],
+    methods: {
+      closeModal() {
+        !this.$emit('close')
+      }
+    }
+
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .modal {
+    width: 25rem;
+    padding: 2rem;
+    margin: 5rem auto;
+    background: white;
+    border-radius: .25rem;
+    box-shadow: 1px 1px 4px -1px grey;
+    cursor: pointer;
+  }
+
+  .backdrop {
+    top: 0;
+    left: 0;
+    position: fixed;
+    background: rgba(0,0,0,.5);
+    width: 100vw;
+    height: 100vh;
+  }
+
+  .sale{
+    background: rgb(245, 245, 255);
+  }
+</style>
